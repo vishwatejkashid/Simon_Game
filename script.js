7e1aef7e17fe0229d4ab8abeb4b547c975bc0316
Vishwatej Kashid <vishwatejkashid18@gmail.com>
2025-07-19 20:17:41 +0530

    button flash

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+let h3 = document.querySelector('h3');
+
 let seq = [];
 let user = [];
+let btns = ["green","red", "yellow","blue"]
 
 let level=0;
 let game = false;
+
+function btnflash(ranbtn) {
+    ranbtn.classList.add("flash")
+    setTimeout(function(){
+        ranbtn.classList.remove("flash");
+    },250);
+}
+
+function levelUp(){
+    level++;
+    h3.innerText = `Level ${level}`
+
+    let ranIdx = Math.floor(Math.random() * 3);
+    let ranColor = btns[ranIdx];
+    let ranbtn = document.querySelector(`.${ranColor}`);
+    console.log(`${ranIdx} , ${ranColor} , ${ranbtn}`)
+    console.dir(ranbtn)
+    btnflash(ranbtn);
+}
 
 document.addEventListener("keypress" , function(ele){
     if(!game && ele.key == "Enter"){
         console.log("game started");
         // console.dir(ele);
         game = true;
+        levelUp();
     }
+
+    
 })
